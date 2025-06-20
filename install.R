5b8dcebb6c15bb1aa5293dc57180497dddd9eff9
delatorre96 <idelatorrecubillo@gmail.com>
2025-06-20 10:30:08 +0200

    r binder

---
+++ install.R
@@ -0,0 +1,8 @@
+install.packages("devtools")
+library(devtools)
+install_github(c("SantanderMetGroup/loadeR.java",
+                 "SantanderMetGroup/climate4R.UDG",
+                 "SantanderMetGroup/loadeR",
+                 "SantanderMetGroup/transformeR",
+                 "SantanderMetGroup/visualizeR",
+                 "SantanderMetGroup/downscaleR"))
